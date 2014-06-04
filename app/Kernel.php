214f28e1162a90588565da878c24c910dbcbbf08
Christopher Davis <cdavis9999@gmail.com>
2014-06-04 16:48:41 -0400

    First commit: skeleton

---
+++ app/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * BBY Open Play
+ *
+ * @package     Chrisguitarguy\\BbyOpenPlay
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+use Silex\Application;
+use Silex\TwigServiceProvider;
+
+class Kernel extends Application
+{
+    private $env;
+
+    public function __construct($env=null)
+    {
+        parent::__construct();
+
+        $this->env = $env ?: 'dev';
+
+        $this->register(new TwigServiceProvider(), [
+            'twig.path'     => __DIR__.'/views',
+            'twig.options'  => function ($app) {
+                return [
+                    'debug'     => $app['debug'],
+                    'cache'     => $app['debug'] ? false : __DIR__.'/cache',
+                ];
+            },
+        ]);
+    }
+}
---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * BBY Open Play
+ *
+ * @package     Chrisguitarguy\\BbyOpenPlay
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+(new Kernel())->run();
